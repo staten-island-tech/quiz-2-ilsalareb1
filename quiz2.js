781511b0c550b6b160d8deb73b252cc08849de80
ilsalareb1 <50841067+ilsalareb1@users.noreply.github.com>
2019-09-12 11:27:51 -0400

    Add files via upload

---
+++ quiz2.js
@@ -0,0 +1,22 @@
+/* let jGame1 = 89; */
+let jGame2 = 120;
+let jGame3 = 103; 
+
+let jGame1 = 300;
+
+const jAverage = ( jGame1 + jGame2 + jGame3 / 3);
+/* console.log(jAverage); */
+
+let mGame1 = 116;
+let mGame2 = 94;
+let mGame3 = 123;
+
+const mAverage = ( mGame1 + mGame2 + mGame3 / 3);
+/* console.log(mAverage); */
+
+if ( jAverage < mAverage) {
+    console.log ( `Mike's average ${mAverage} is higher than John's ${jAverage}.`);
+} else {
+    console.log (`John's average ${jAverage} is higher than Mike's ${mAverage}.`);
+}
+
